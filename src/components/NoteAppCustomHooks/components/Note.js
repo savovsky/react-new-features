323dfc983b21721dced201ff7894eef09ace6b92
Miroslav Savovski <miroslav.savovski@scalefocus.com>
2019-05-31 10:04:52 +0300

    custom Hooks end.

--- src/components/NoteAppCustomHooks/components/Note.js
+++ src/components/NoteAppCustomHooks/components/Note.js
@@ -1,14 +1,19 @@
 import React, { useContext } from 'react';
 import { Button } from '@material-ui/core';
 import Context from '../context/notes-context';
+import useMousePosition from '../hooks/useMousePosition';
+
 
 const NoteContext = ({ note }) => {
   const { notesDispatch } = useContext(Context);
+
+  const position = useMousePosition();
 
   return (
     <div>
       <h3>{note.title}</h3>
       <p>{note.body}</p>
+      <p>{position.x}, {position.y}</p>
       <Button
         variant="contained"
         color="primary"
